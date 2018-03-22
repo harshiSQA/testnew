e4396c7aeafb5c4265af96cf036ddc9324e66fc4
harshiSQA <37262164+harshiSQA@users.noreply.github.com>
2018-03-22 22:58:41 +0530

    first version of hungry code

---
+++ Character.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Character{
+public static void main(String[] args) {
+    Scanner in=new Scanner(System.in);
+    String s;
+    System.out.println("Enter a line:");
+    s=in.nextLine();
+	
+   // System.out.println("total  number of words start with capital letters are :"+ct);
+}}
